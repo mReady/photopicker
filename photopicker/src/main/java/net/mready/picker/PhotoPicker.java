d3c8f959f784ff4b29428a2124c3dbe6992362e3
Bogdan Mihai <bogdan.mihai@mready.net>
2017-07-31 12:27:49 +0300

    Renamed app module in sample; Changed builder pattern;

--- photopicker/src/main/java/net/mready/picker/PhotoPicker.java
+++ photopicker/src/main/java/net/mready/picker/PhotoPicker.java
@@ -10,41 +10,49 @@ public final class PhotoPicker {
 
     public static class Builder {
 
-        private final Intent photoPickerIntent;
         private final Context context;
+        private CharSequence title;
+        private int maxWidth = -1;
+        private int maxHeight = -1;
+        private int quality = 70;
 
         public Builder(Context context) {
             this.context = context;
-            photoPickerIntent = new Intent(context, PhotoPickerActivity.class);
         }
 
         public Builder maxWidth(int maxWidth) {
-            photoPickerIntent.putExtra(PhotoPickerActivity.EXTRA_REQ_WIDTH, maxWidth);
+            this.maxWidth = maxWidth;
             return this;
         }
 
         public Builder maxHeight(int maxHeight) {
-            photoPickerIntent.putExtra(PhotoPickerActivity.EXTRA_REQ_HEIGHT, maxHeight);
+            this.maxHeight = maxHeight;
             return this;
         }
 
         public Builder quality(int quality) {
-            photoPickerIntent.putExtra(PhotoPickerActivity.EXTRA_COMPRESSION_QUALITY, quality);
+            this.quality = quality;
             return this;
         }
 
         public Builder title(int resId) {
-            photoPickerIntent.putExtra(PhotoPickerActivity.EXTRA_TITLE, context.getString(resId));
+            this.title = context.getString(resId);
             return this;
         }
 
         public Builder title(CharSequence title) {
-            photoPickerIntent.putExtra(PhotoPickerActivity.EXTRA_TITLE, title);
+            this.title = title;
             return this;
         }
 
         public Intent build() {
+            Intent photoPickerIntent = new Intent(context, PhotoPickerActivity.class);
+            photoPickerIntent.putExtra(PhotoPickerActivity.EXTRA_REQ_WIDTH, maxWidth);
+            photoPickerIntent.putExtra(PhotoPickerActivity.EXTRA_REQ_HEIGHT, maxHeight);
+            photoPickerIntent.putExtra(PhotoPickerActivity.EXTRA_COMPRESSION_QUALITY, quality);
+            photoPickerIntent.putExtra(PhotoPickerActivity.EXTRA_TITLE, title);
             return photoPickerIntent;
+
         }
 
     }
